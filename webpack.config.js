217cfffa96911d30b72923786302cd88cfcdb717
markellekelly <mkelly23@calpoly.edu>
2019-08-02 17:18:29 -0700

    REAL DEALIO

--- webpack.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  entry: ['./clarity/static/index.js'],
-  output: {
-    path:require('path').join(__dirname, 'clarity','static'),
-    filename: 'bundle.js'
-  },
-  mode: 'development'
-}
